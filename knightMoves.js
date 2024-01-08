0077bed80b4819f9fb087c1fe9d0763cd47fa777
MihailPetkov96 <mpetkov228@gmail.com>
2024-01-08 22:38:53 +0200

    Add function to calculate all possible moves for given [x, y]

--- knightMoves.js
+++ knightMoves.js
@@ -2,4 +2,15 @@ function Node(pos, path) {
     if (pos[0] >= 0 && pos[0] <= 7 && pos[1] >= 0 && pos[1] <= 7) {
         return { pos, path };
     }
+
+    return null;
+}
+
+function calcMoves([x, y]) {
+    return [
+        [x + 2, y + 1], [x + 1, y + 2],
+        [x - 1, y + 2], [x - 2, y + 1],
+        [x - 2, y - 1], [x - 1, y - 2],
+        [x + 1, y - 2], [x + 2, y - 1],
+    ];
 }
