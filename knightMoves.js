9dc38276d45b2035ab953db0a03d3a4c2c22fb00
MihailPetkov96 <mpetkov228@gmail.com>
2024-01-09 23:25:46 +0200

    Add knightMoves function

--- knightMoves.js
+++ knightMoves.js
@@ -14,3 +14,28 @@ function calcMoves([x, y]) {
         [x + 1, y - 2], [x + 2, y - 1],
     ];
 }
+
+function knightMoves([x, y], [a, b]) {
+    let queue = [Node([x, y], [[x, y]])];
+    let currentNode = queue.shift();
+
+    while (currentNode.pos[0] != a || currentNode.pos[1] != b) {
+        let moves = calcMoves(currentNode.pos);
+        for (let move of moves) {
+            let node = Node(move, currentNode.path.concat([move]));
+            if (node) {
+                queue.push(node);
+            }
+        }
+        currentNode = queue.shift();
+    }
+
+    let pathString = '';
+    for (let path of currentNode.path) {
+        pathString += `[${path}] `;
+    }
+
+    console.log(`Moves: ${currentNode.path.length - 1}  Path: ${pathString}`);
+}
+
+knightMoves([0, 0], [4, 4]);
